7ab120350efef72c1e732f01c205c3cf14cbea8b
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2025-02-24 11:40:23 +0800

    feat: Create Post Item for Home screen

--- composeApp/src/commonMain/kotlin/com/mnhyim/haxxernyius/ui/home/Home.kt
+++ composeApp/src/commonMain/kotlin/com/mnhyim/haxxernyius/ui/home/Home.kt
@@ -1,13 +1,23 @@
 package com.mnhyim.haxxernyius.ui.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Home(
@@ -20,6 +30,80 @@ fun Home(
             Text(
                 text = "Home",
                 style = MaterialTheme.typography.titleLarge
+            )
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(0.dp),
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                items(20) {
+                    PostItem(
+                        item = it,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {}
+                            .padding(16.dp)
+                    )
+                    HorizontalDivider()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PostItem(
+    item: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Text(
+            text = "$item.",
+            style = MaterialTheme.typography.titleSmall
+        )
+        Column(
+            modifier = Modifier.weight(1f).padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Adding Mastodon Comments to Your Blog (beej.us)",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Row {
+                Text(
+                    text = "Dhouston | ",
+                    style = MaterialTheme.typography.labelSmall.copy(
+                        color = MaterialTheme.colorScheme.onSurface.copy(
+                            alpha = 0.35f
+                        )
+                    )
+                )
+                Text(
+                    text = "27 January 2020 | ",
+                    style = MaterialTheme.typography.labelSmall.copy(
+                        color = MaterialTheme.colorScheme.onSurface.copy(
+                            alpha = 0.35f
+                        )
+                    )
+                )
+                Text(
+                    text = "32 Replies |",
+                    style = MaterialTheme.typography.labelSmall.copy(
+                        color = MaterialTheme.colorScheme.onSurface.copy(
+                            alpha = 0.35f
+                        )
+                    )
+                )
+            }
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "")
+            Text(
+                text = "(121)",
+                style = MaterialTheme.typography.labelSmall
             )
         }
     }
